6be90d8781e89be8bddb0bd0dc438be60b4c9e66
vlGoku <markolucic21@outlook.de>
2023-05-26 10:06:55 +0200

    second commit

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 const text = document.querySelector(".text");
 const wert = document.querySelector(".wert");
+const ausgabe = document.querySelector(".ausgabe");
+const task = document.querySelector(".verschluesseln");
+const neu = document.querySelector(".reset");
 
 const num = parseInt(wert.value);
-const input = text.value;
-myArray = input.split("");
+const inp = text.value;
+myArray = inp.split("");
 
 function letter(letter, num) {
   let ergebnis = letter.charCodeAt();
   ergebnis += num;
   ergebnis = String.fromCharCode(ergebnis);
-  console.log(ergebnis);
+  return ergebnis;
 }
 function shiftChar(myArray, num) {
+  let shiftedLetters = [];
   for (let i = 0; i < myArray.length; i++) {
-    letter(myArray[i], num);
+    shiftedLetters.push(letter(myArray[i], num));
   }
+  let test = shiftedLetters.join("");
+  ausgabe.textContent = test;
 }
+task.addEventListener("click", function () {
+  console.log("geklickt");
+  shiftChar(myArray, num);
+});
+neu.addEventListener("click", () => {
+  wert.value = "";
+  text.value = "";
+  location.reload();
+});
+
+/* num % 26 === 0 ? (num = 26) : (num = num % 26);
+ */
